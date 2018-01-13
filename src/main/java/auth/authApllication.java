25d3ecf509878f4a0bf54cb096a9085d5363a720
Saddam Shaik <saddam.shaik@gdn-commerce.com>
2018-01-13 18:24:19 +0530

    added auth , added order

---
+++ src/main/java/auth/authApllication.java
@@ -0,0 +1,11 @@
+package auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class authApllication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(auth.authApllication.class, args);
+    }
+}
---
+++ src/main/java/order/orderApplication.java
@@ -0,0 +1,10 @@
+package order;
+
+import org.springframework.boot.SpringApplication;
+
+public class orderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(order.orderApplication.class, args);
+    }
+}
