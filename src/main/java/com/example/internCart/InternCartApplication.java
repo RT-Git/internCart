93b5fc4da07a2dde0aa4bc60efae5bb6140e966f
backUp <backup@Coviams-MacBook-Pro.local>
2018-01-13 17:29:14 +0530

    Initial Commit

---
+++ src/main/java/com/example/internCart/InternCartApplication.java
@@ -0,0 +1,12 @@
+package com.example.internCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternCartApplication.class, args);
+	}
+}
